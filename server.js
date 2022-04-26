197690b0bb7910703e370417d018ab8f6393704d
Abraham Alonge <aalonge@unc.edu>
2022-04-26 00:11:55 -0400

    Define check endpoint

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+	res.statusCode = 200;
+    // Respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+    res.end(res.statusCode+ ' ' +res.statusMessage)
+});
+
 // Default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
