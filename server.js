ee4b6392ac5d1c27674551112f391a1e5e266fae
Abraham Alonge <aalonge@unc.edu>
2022-04-26 00:37:10 -0400

    Add flips endpoint

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.get('/app/', (req, res) => {
     res.end(res.statusCode+ ' ' +res.statusMessage)
 });
 
+app.get('/app/flips/:number', (req, res) => {
+	let raw = coin.coinFlips(req.params.number)
+    let summary = coin.countFlips(raw)
+    let result = {'raw': raw, 'summary': summary}
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.set('Content-Type', 'text/json');
+    res.json(result);
+});
+
 // Default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
