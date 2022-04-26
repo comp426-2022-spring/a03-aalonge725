0ce480a8473c0aefec26618f45f82d3a70d2bf0e
Abraham Alonge <aalonge@unc.edu>
2022-04-26 00:06:41 -0400

    Define default endpoint

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ const port = args.port || process.env.PORT || 5000
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
